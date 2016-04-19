a34c79ec71153b4a3ce72fe9911290448c482ab0
Pierre Smith <smith.pierre@me.com>
2016-04-19 14:59:52 -0400

    fixed some more rspec tests

--- spec/views/chapters/index.html.erb_spec.rb
+++ spec/views/chapters/index.html.erb_spec.rb
@@ -1,34 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe "chapters/index", type: :view do
+
   before(:each) do
+    @story = assign(:story, Story.create!(
+    :title => "StoryTitle",
+    :description => "MyText"
+    ))
     assign(:chapters, [
       Chapter.create!(
-        :title => "Title",
+        :title => "ChapterTitle",
         :position => 1,
         :video => "Video",
         :pdf => "Pdf",
         :audio => "Audio",
-        :story => nil
+        :story => @story
       ),
       Chapter.create!(
-        :title => "Title",
+        :title => "ChapterTitle",
         :position => 1,
         :video => "Video",
         :pdf => "Pdf",
         :audio => "Audio",
-        :story => nil
+        :story => @story
       )
     ])
   end
 
   it "renders a list of chapters" do
     render
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
+    assert_select "tr>td", :text => "ChapterTitle".to_s, :count => 2
     assert_select "tr>td", :text => 1.to_s, :count => 2
     assert_select "tr>td", :text => "Video".to_s, :count => 2
     assert_select "tr>td", :text => "Pdf".to_s, :count => 2
     assert_select "tr>td", :text => "Audio".to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select "tr>td", :text => "StoryTitle".to_s, :count => 2
   end
 end
